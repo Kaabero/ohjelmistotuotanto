7974ad3499f0b5f77ff6fd7be8343f6954aa12d1
Katri <katri.laamala@outlook.com>
2024-11-05 10:48:20 +0200

    uusi branch tulo

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -1,16 +1,15 @@
-# tehdään alussa importit
-
 from logger import logger
 from summa import summa
 from erotus import erotus
+from tulo import tulo
 
-logger("aloitetaan ohjelma") # muutos mainissa
+logger("aloitetaan ohjelma")
 
 x = int(input("luku 1: "))
 y = int(input("luku 2: "))
-
-print(f"Summa: {x} + {y} = {summa(x, y)}") # merge
-print(f"Summa:{x} - {y} = {erotus(x, y)}") # merge
+print(f"{x} + {y} = {summa(x, y)}") 
+print(f"{x} - {y} = {erotus(x, y)}") 
+print(f"{x} * {y} = {tulo(x, y)}") 
 
 logger("lopetetaan")
-print("goodbye!") # lisäys bugikorjaus-branchissa
+print("goodbye!")
