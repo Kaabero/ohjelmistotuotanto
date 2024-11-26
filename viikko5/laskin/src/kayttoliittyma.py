27fcac3876ac6bef3a12ecff415d0839502f4d6a
Katri <katri.laamala@outlook.com>
2024-11-26 08:41:27 +0200

    tehtävä 5

---
+++ viikko5/laskin/src/kayttoliittyma.py
@@ -0,0 +1,128 @@
+from enum import Enum
+from tkinter import ttk, constants, StringVar
+
+
+class Komentotehdas:
+    def __init__(self, sovelluslogiikka):
+        self.sovelluslogiikka = sovelluslogiikka
+
+
+        self._komennot = {
+            Komento.SUMMA: Summa(self.sovelluslogiikka),
+            Komento.EROTUS: Erotus(self.sovelluslogiikka),
+            Komento.NOLLAUS: Nollaus(self.sovelluslogiikka),
+            Komento.KUMOA: Kumoaminen(self.sovelluslogiikka)
+        }
+   
+    def hae(self, komento):
+        if komento in self._komennot:
+            return self._komennot[komento]
+
+
+class Komento(Enum):
+    SUMMA = 1
+    EROTUS = 2
+    NOLLAUS = 3
+    KUMOA = 4
+
+
+class Summa:
+    def __init__(self, sovelluslogiikka):
+        self.sovelluslogiikka = sovelluslogiikka
+
+    def suorita(self, arvo):
+        self.sovelluslogiikka.plus(arvo)
+
+class Erotus:
+    def __init__(self, sovelluslogiikka):
+        self.sovelluslogiikka = sovelluslogiikka
+
+    def suorita(self, arvo):
+        self.sovelluslogiikka.miinus(arvo)
+
+
+class Nollaus:
+    def __init__(self, sovelluslogiikka):
+        self.sovelluslogiikka = sovelluslogiikka
+
+    def suorita(self, arvo):
+        self.sovelluslogiikka.nollaa()
+
+class Kumoaminen:
+    def __init__(self, sovelluslogiikka):
+        self.sovelluslogiikka = sovelluslogiikka
+
+    def suorita(self, arvo):
+        pass
+
+
+class Kayttoliittyma:
+    def __init__(self, sovelluslogiikka, root):
+        self._sovelluslogiikka = sovelluslogiikka
+        self._root = root
+        self.komennot = Komentotehdas(self._sovelluslogiikka)
+
+    def kaynnista(self):
+        self._arvo_var = StringVar()
+        self._arvo_var.set(self._sovelluslogiikka.arvo())
+        self._syote_kentta = ttk.Entry(master=self._root)
+
+        tulos_teksti = ttk.Label(textvariable=self._arvo_var)
+
+        summa_painike = ttk.Button(
+            master=self._root,
+            text="Summa",
+            command=lambda: self._suorita_komento(Komento.SUMMA)
+        )
+
+        erotus_painike = ttk.Button(
+            master=self._root,
+            text="Erotus",
+            command=lambda: self._suorita_komento(Komento.EROTUS)
+        )
+
+        self._nollaus_painike = ttk.Button(
+            master=self._root,
+            text="Nollaus",
+            state=constants.DISABLED,
+            command=lambda: self._suorita_komento(Komento.NOLLAUS)
+        )
+
+        self._kumoa_painike = ttk.Button(
+            master=self._root,
+            text="Kumoa",
+            state=constants.DISABLED,
+            command=lambda: self._suorita_komento(Komento.KUMOA)
+        )
+
+        tulos_teksti.grid(columnspan=4)
+        self._syote_kentta.grid(columnspan=4, sticky=(constants.E, constants.W))
+        summa_painike.grid(row=2, column=0)
+        erotus_painike.grid(row=2, column=1)
+        self._nollaus_painike.grid(row=2, column=2)
+        self._kumoa_painike.grid(row=2, column=3)
+
+         
+
+    def _suorita_komento(self, komento):
+
+
+        arvo = 0
+
+        try:
+            arvo = int(self._syote_kentta.get())
+        except Exception:
+            pass
+
+        self.komennot.hae(komento).suorita(arvo)
+
+
+        self._kumoa_painike["state"] = constants.NORMAL
+
+        if self._sovelluslogiikka.arvo() == 0:
+            self._nollaus_painike["state"] = constants.DISABLED
+        else:
+            self._nollaus_painike["state"] = constants.NORMAL
+
+        self._syote_kentta.delete(0, constants.END)
+        self._arvo_var.set(self._sovelluslogiikka.arvo())
