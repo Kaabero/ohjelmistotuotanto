c96c6d2a7123b78cbd89c259f97ab849c5908d1b
Katri <katri.laamala@outlook.com>
2024-12-02 09:16:53 +0200

    tehtävä 3

--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -9,6 +9,21 @@ class And:
 
         return True
 
+class Or:
+    def __init__(self, *matchers):
+        self._matchers = matchers
+        
+
+    def test(self, player):
+        count=0
+        for matcher in self._matchers:
+            if matcher.test(player):
+                count+=1
+
+        if count>0:
+            return True
+        return False
+
 
 class PlaysIn:
     def __init__(self, team):
