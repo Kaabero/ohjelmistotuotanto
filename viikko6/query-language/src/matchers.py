447d2288d97dfdad4404b487edb8e37880e86eb3
Katri <katri.laamala@outlook.com>
2024-12-02 10:03:24 +0200

    tehtävät 4 ja 5

--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -69,3 +69,28 @@ class All:
 
 
 
+class QueryBuilder:
+    def __init__(self, matcher=All()):
+        self._matcher = matcher
+
+    def plays_in(self, team):
+        return QueryBuilder(And(self._matcher, PlaysIn(team)))
+
+    def has_at_least(self, value, attr):
+        return QueryBuilder(And(self._matcher, HasAtLeast(value, attr)))
+
+    def has_fewer_than(self, value, attr):
+        return QueryBuilder(And(self._matcher, HasFewerThan(value, attr)))
+
+    def one_of(self, *matchers):
+        return QueryBuilder(Or(*matchers))
+
+    def not_(self, condition):
+        return QueryBuilder(And(self._matcher, Not(condition)))
+
+    def build(self):
+        return self._matcher
+
+
+
+
